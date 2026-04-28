b18dff58beee66a90bff0c4fefdac6fa134591e9
maicoltascon <viejo.miken@gmail.com>
2026-04-28 12:14:32 -0500

    Se creo las paginas de contratos y contratista

---
+++ src/app/pages/contractor/contractor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contracts',
+  standalone: true,
+  imports: [],
+  templateUrl: './contractor.component.html',
+  styleUrl: './contractor.component.scss'
+})
+export class ContractorComponent {
+
+}
---
+++ src/app/pages/contracts/contracts.component.ts
@@ -0,0 +1,12 @@
+import ( Component ) from "angular/core '; 373.2k (gzipped : 11o.7k)
+
+@Component ( {
+    selector: 'app-contracts',
+    standalone: true,
+    imports: [],
+    templateUrl: ',contracts.components.html',
+    styleUrl: ',contracts.components.scss',
+})
+export class ContractsComponents (
+
+)
