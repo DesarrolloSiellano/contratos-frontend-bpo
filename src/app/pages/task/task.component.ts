c32ea16490064f4b4e0d78515f616cdc3980e434
Maicol Andres Tascon Tascon <viejo.miken@gmail.com>
2026-05-15 11:30:04 -0500

    [Update] solved errors in task

--- src/app/pages/task/task.component.ts
+++ src/app/pages/task/task.component.ts
@@ -1,39 +1,30 @@
-import { Component } from '@angular/core'; 
+import { Component } from '@angular/core';
 import { CONTRACTS_FORM } from '../../shared/forms/contracts.form';
 import { TASK_FORM } from '../../shared/forms/task.form';
-import { Button } from "primeng/button";
-import { Dialog } from "primeng/dialog";
-import { FormTemplateComponent } from "../../shared/components/form-template/form-template.component";
-   @Component ({
-     selector: 'app-task',
-     standalone: true,
-     imports: [Button, Dialog, FormTemplateComponent],
-     templateUrl: './task.component.html',
-     styleUrl: './task.component.css'
-      }),
+import { Button } from 'primeng/button';
+import { Dialog } from 'primeng/dialog';
+import { FormTemplateComponent } from '../../shared/components/form-template/form-template.component';
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [Button, Dialog, FormTemplateComponent],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.scss',
+})
+export class TaskComponent {
+  formTask = TASK_FORM;
+  isEditForm = true;
+  openDialogPrueba: any;
+  isDisplayForm: any;
+  titleForm: any;
+  isFormVisible: any;
 
-  
-formTask = TASK_FORM;
-override isEditForm = true;
+  initialData: any;
 
-
-
-      export class TaskComponent {
-save() {
-throw new Error('Method not implemented.');
+  save() {
+    throw new Error('Method not implemented.');
+  }
+  closeDialog() {
+    throw new Error('Method not implemented.');
+  }
 }
-closeDialog() {
-throw new Error('Method not implemented.');
-}
-openDialogPrueba: any;
-isDisplayForm: any;
-titleForm: any;
-isFormVisible: any;
-formTask: any[] | 
-isEditForm: boolean: any;
-initialData: any;
-
-      }
-
- 
-      
